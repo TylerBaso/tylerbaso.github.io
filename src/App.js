ef8e8726909f63268143fe3e11f817785c1e2ccf
Tyler Baso <basotyler@gmail.com>
2019-07-18 19:29:03 -0400

    james

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logo from './james.svg'
 import './App.css';
 
-function App() {
+function Index() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +21,40 @@ function App() {
         </a>
       </header>
     </div>
+    );
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+      </div>
+    </Router>
   );
 }
 
